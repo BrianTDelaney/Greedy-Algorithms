14a248c2d8ddf81e3a34db90df3b4116127f3be7
BradenAzis <bradenkai747@gmail.com>
2026-03-05 16:39:43 -0500

    Initial file template

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
 
+using namespace std;
+
+// TODO: Insert FIFO declaration
+// TODO: Insert LRU declaration
+int optff(int k, vector<int> &requests);
+
+
+int main() {
+
+    return 0;
+}
+
+
+
+// TODO: Insert FIFO definition
+
+// TODO: Insert LRU definition
+
+int optff(int k, vector<int> &requests) {
+    // Parameters:
+        // k: cache capacity
+        // requests: reference to request sequence array
+
+    // Return:
+        // misses: integer value of the number of misses
+
+    vector<int> cache;
+    int misses = 0;
+    
+    // iterate through request array
+    for (int i = 0; i < requests.size(); i++) {
+
+    }
+
+
+    return misses;
+}
